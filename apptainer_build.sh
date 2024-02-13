389e25fa898845b2840de6e5247b8043c6595978
Ste O'Brien <stephan.obrien@mcgill.ca>
2024-02-13 15:21:46 -0500

    ci(apptainer): refactored scripts
    renamed scripts and refactored to get apptainer/singularity depending which is installed

--- apptainer_build.sh
+++ apptainer_build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-apptainer build ./gammapy-tools.sif singularity.def
+if command -v apptainer &> /dev/null
+then
+    apptainer build ./gammapy-tools.sif gammapy-tools.def
+else
+    singularity build ./gammapy-tools.sif gammapy-tools.def
+fi
