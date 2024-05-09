41b04df1c83d84fd37644211d081a77b8b0c49f0
Ste O'Brien <stephan.obrien@mcgill.ca>
2024-05-09 11:11:27 -0400

    fix(pyproject.toml): fixing versioning

--- gammapy_tools/__init__.py
+++ gammapy_tools/__init__.py
@@ -2,10 +2,12 @@ from . import analysis
 from . import make_background
 from . import templates
 from . import fake_source_coordinates
-
+from ._version import __version__
 __all__ = (
     analysis.__all__
     + make_background.__all__
     + templates.__all__
     + fake_source_coordinates.__all__
+    + ["__version__"]
 )
+
